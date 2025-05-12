c6a44b135c0c48acc9a60cc3926d698f20dc0d59
Alisa Novik <alyssa.novik@gmail.com>
2025-05-12 14:12:30 -0400

    setup.sh update

--- setup.sh
+++ setup.sh
@@ -10,3 +10,7 @@ ln -s ~/dotfiles/.config/nvim/init.lua ~/.config/nvim/init.lua
 ln -s ~/dotfiles/.config/fish/ ~/.config/fish
 ln -s ~/dotfiles/.config/kitty/ ~/.config/kitty
 ln -s ~/dotfiles/.config/polybar/ ~/.config/polybar
+
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
