f290ecc99e6fddc4e4ec3bc17f8119b2d986f9bd
Alisa Novik <alyssa.novik@gmail.com>
2025-05-13 23:14:32 -0400

    dotfiles?

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
 # Install musthave software
-sudo apt-get install -y kitty fish polybar tmux gcc make unzip
+sudo apt-get install -y kitty fish polybar tmux gcc make unzip picom rofi \
+				build-essential ripgrep fd-find xclip fonts-noto-color-emoji \
+				ninja-build gettext cmake unzip curl 
 
 # Link dotfiles
 ln -s ~/dotfiles/.config/i3/config ~/.config/i3/config
@@ -10,6 +12,7 @@ ln -s ~/dotfiles/.config/nvim/init.lua ~/.config/nvim/init.lua
 ln -s ~/dotfiles/.config/fish/ ~/.config/fish
 ln -s ~/dotfiles/.config/kitty/ ~/.config/kitty
 ln -s ~/dotfiles/.config/polybar/ ~/.config/polybar
+ln -s ~/dotfiles/.config/picom/ ~/.config/picom
 
 git config --global alias.co checkout
 git config --global alias.ci commit
